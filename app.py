ef992597372ffe0801a6b40f3852d1b18ec45d92
kdenny25 <kelsead25@gmail.com>
2023-04-14 12:05:36 -0400

    Created menu management system. There are still a few things that need to be added but it works enough to get the menu working.

--- app.py
+++ app.py
@@ -11,12 +11,15 @@ import os, sys
 import json
 import datetime
 from backend.user import User
+from werkzeug.utils import secure_filename
 
 base_dir = '.'
 
 app = Flask(__name__,
             static_folder=os.path.join(base_dir, 'static'),
             template_folder=os.path.join(base_dir, 'templates'),)
+
+app.config['UPLOAD_FOLDER'] = os.path.join(base_dir, "static/uploaded_images/")
 
 # User session management setup
 login_manager = LoginManager()
@@ -25,12 +28,15 @@ login_manager.init_app(app)
 # Hashing method for user passwords
 bcrypt = Bcrypt(app)
 
+# csrf protection
 csrf = CSRFProtect(app)
 
 # todo: allow login with google credentials
 # OAuth 2 client setup
 # gClient = WebApplicationClient(GOOGLE_CLIENT_ID)
 
+# checks if local development environment or production environment and
+# connects to correct MongoDB database.
 if 'COSMOS_CONNECTION_STRING' not in os.environ:
     # local development, where environment variables are used
     print('Loading config.development and environment variables from .env file.')
@@ -43,11 +49,17 @@ else:
     conn_str = app.config.get('CONN_STRING')
     client = MongoClient(conn_str)
 
+# sets up database
 db = client.pizza_db
 
+# database collections
 orders = db.orders
 getInTouch = db.getInTouch
 users = db.users
+menu_categories = db.menuCategories
+menu_dishes = db.menuDishes
+beverages = db.menuBeverages
+toppings = db.menuToppings
 
 # this is called when the user is loaded into a webpage
 @login_manager.user_loader
@@ -58,47 +70,61 @@ def load_user(user_id):
     else:
         return None
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def home_page():
-    if request.method == 'POST':
-        return redirect('index.html')
-    else:
-        print(current_user)
-        return render_template('index.html')
+    # opens homepage
+    return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
+        # gets data from form when POST method is passed
         email = request.form.get('email')
         password = request.form.get('password')
 
-        #pulls all user details from users collection
+        # finds user in database and assigns profile details to find_user
         find_user = users.find_one({'email': email})
+
+        # if user successfully found check if password is correct and log user in
         if find_user is not None:
             if bcrypt.check_password_hash(find_user['password'], password):
                 # assign user details (in JSON format) to the User class
                 log_user = User(find_user)
                 login_user(log_user)
+
+                # redirect to homepage
                 return redirect('/')
             else:
+                # passes an error message to login page.
                 flash('Password is incorrect.', 'error')
 
+        # passes error message to login page
         flash('User does not exist.', 'error')
+
+        # if user does not exist returns user to login page with error message
         return render_template('elements/login_modal.html')
 
+    # default GET response is to load the login page
     return render_template('elements/login_modal.html')
 
 @app.route('/signup', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
+        # gets data from the form
         fName = request.form.get('fName')
         lName = request.form.get('lName')
         email = request.form.get('email')
+
+        # default assignment for role
         role = 'customer'
+
+        # encrypt password before saving to database
         password = bcrypt.generate_password_hash(request.form.get('password')).decode('utf-8')
 
+        # find if user email already exists
         find_user = users.find_one({'email': email})
 
+        # if email doesn't already exist then create new user
         if find_user is None:
             _id = users.insert_one({'first_name': fName,
                                     'last_name': lName,
@@ -107,20 +133,26 @@ def register():
                                     'password': password})
             # sends message if registration is successful
             flash(f'Account created for {fName}!', 'success')
+            # returns user to homepage with success message
             return redirect('/')
         else:
+            # if user already exists sends error message
             flash(f'Account already exits for email: {email}!', 'error')
+
+    # default response is to load registration page.
     return render_template('elements/register_modal.html')
 
 @app.route('/logout')
 @login_required
 def logout():
+    # log user out and redirect to home page
     logout_user()
     return redirect("/")
 
 @app.route('/admin_dashboard')
 @login_required
 def admin_dash():
+    # checks if user is admin before loading page.
     if current_user.is_admin:
         return render_template("admin_dashboard/admin_dashboard.html")
     else:
@@ -189,11 +221,168 @@ def admin_delete_profile():
     if current_user.is_admin:
         id = request.values.get("_id")
         users.delete_one({'_id': ObjectId(id)})
-        flash('Profile: ' + str(id) + ' successfully deleted.')
+        flash('Profile: ' + str(id) + ' successfully deleted.', 'success')
         return redirect('/user_management')
     return redirect('/')
 
+@app.route('/menu_management', methods=['GET', 'POST'])
+def admin_menu_management():
+    if current_user.is_admin:
+        if request.method == 'GET':
+            count_cats = menu_categories.count_documents({})
+            count_bevs = beverages.count_documents({})
+            count_tops = toppings.count_documents({})
+            count_dishes = menu_dishes.count_documents({})
 
+            dish_categories = list(menu_categories.find({'category_type': 'dish'}))
+            beverage_categories = list(menu_categories.find({'category_type': 'beverage'}))
+            topping_categories = list(menu_categories.find({'category_type': 'topping'}))
+
+            bev_options = list(beverages.find())
+            dish_options = list(menu_dishes.find())
+
+            bev_dict = {}
+
+            for bev_cat in beverage_categories:
+                bev_list = []
+                for bev in bev_options:
+                    if bev['category'] == bev_cat['category']:
+                        bev_list.append(bev)
+                bev_cat['bev_list'] = bev_list
+
+            for dish_cat in dish_categories:
+                dish_list = []
+                for dish in dish_options:
+                    if dish['category'] == dish_cat['category']:
+                        dish_list.append(dish)
+                dish_cat['dish_list'] = dish_list
+
+            counts = {'menu_items': count_dishes,
+                      'categories': count_cats,
+                      'beverages': count_bevs,
+                      'toppings': count_tops}
+
+            page_data = {'counts': counts,
+                         'dish_categories': dish_categories,
+                         'bev_categories': beverage_categories,
+                         'top_categories': topping_categories}
+
+
+            return render_template('admin_dashboard/admin_menu_management.html', menu_data=page_data)
+    return redirect('/')
+
+@app.post('/add_menu_category')
+def add_menu_category():
+    if current_user.is_admin:
+        category_type = request.form.get('categoryType')
+        category = request.form.get('categoryName')
+
+        find_category = menu_categories.find_one({'category_type': category_type,
+                                                  'category': category})
+
+        if find_category is None:
+            menu_categories.insert_one({'category_type': category_type,
+                                        'category': category})
+            flash('Category ' + category + ' successfully added.', 'success')
+            return redirect('/menu_management')
+        else:
+            flash('Category already exists', 'error')
+            return redirect('/menu_management')
+    else:
+        return redirect('/')
+
+@app.post('/add_menu_beverage')
+def add_menu_beverage():
+    if current_user.is_admin:
+        bev_name = request.form.get('beverageName')
+        bev_image = request.files.get('beverageImage', None)
+        bev_image_filepath = os.path.join(app.config['UPLOAD_FOLDER'],
+                                          secure_filename(bev_image.filename))
+        bev_image.save(bev_image_filepath)
+        bev_category = request.form.get('beverageCat')
+        bev_description = request.form.get('beverageDescription')
+        bev_cost = float(request.form.get('beverageCost'))
+        bev_price = float(request.form.get('beveragePrice'))
+        bev_net_profit = bev_price - bev_cost
+
+        find_beverage = beverages.find_one({'beverage_name': bev_name})
+
+        if find_beverage is None:
+            beverages.insert_one({'name': bev_name,
+                                  'image': bev_image_filepath,
+                                  'category': bev_category,
+                                  'dscription': bev_description,
+                                  'cost': bev_cost,
+                                  'price': bev_price,
+                                  'net_profit': bev_net_profit})
+            flash('Beverage successfully added.', 'success')
+            redirect('/menu_management')
+        else:
+            flash('Beverage name already exists')
+            redirect('/menu_management')
+
+        return redirect('/menu_management')
+    else:
+        return redirect('/')
+
+@app.post('/add_menu_topping')
+def add_menu_topping():
+    if current_user.is_admin:
+        top_name = request.form.get('toppingName')
+        top_category = request.form.getlist('toppingCategory')
+
+        find_topping = toppings.find_one({'name': top_name})
+
+        if find_topping is None:
+            _id= toppings.insert_one({'name': top_name,
+                                'categories': top_category})
+
+            for cat in top_category:
+                menu_categories.update_one({'category': cat}, {'$push': {
+                                                                'toppings': {
+                                                                    'top_name': top_name
+                                                                }}})
+            flash('Topping successfully added.', 'success')
+            return redirect('/menu_management')
+
+        flash('Topping already exists.', 'error')
+        return redirect('/menu_management')
+    else:
+        return redirect('/')
+
+@app.post('/add_menu_dish')
+def add_menu_dish():
+    if current_user.is_admin:
+        dish_name = request.form.get('dishName')
+        dish_image = request.files.get('dishImage', None)
+        dish_image_filepath = os.path.join(app.config['UPLOAD_FOLDER'],
+                                          secure_filename(dish_image.filename))
+        dish_image.save(dish_image_filepath)
+        dish_category = request.form.get('dishCat')
+        dish_description = request.form.get('dishDescription')
+        dish_cost = float(request.form.get('dishCost'))
+        dish_price = float(request.form.get('dishPrice'))
+        dish_net_profit = dish_price - dish_cost
+
+        find_dish = menu_dishes.find_one({"name": dish_name})
+
+        if find_dish is None:
+            menu_dishes.insert_one({'name': dish_name,
+                                    'image': dish_image_filepath,
+                                    'category': dish_category,
+                                    'description': dish_description,
+                                    'cost': dish_cost,
+                                    'price': dish_price,
+                                    'net_profit': dish_net_profit})
+            flash('Dish added successfully', 'success')
+            return redirect('/menu_management')
+        else:
+            flash('Dish already exists', 'error')
+            return redirect('/menu_management')
+    else:
+        return redirect('/')
+
+# inserts communication responses from customers
 @app.post('/get_in_touch')
 def get_in_touch():
     name = request.form.get('name')

87dacd79a949ea6cfb45004f1d71ad837f40f206
kdenny25 <kelsead25@gmail.com>
2023-04-28 15:16:58 -0400

    Updated menu a little bit so the dishes look uniform and fixed the min max values on the front end.

--- backend/site_logging.py
+++ backend/site_logging.py
@@ -4,20 +4,24 @@ import json
 import datetime
 
 def log_site_traffic(db):
-    site_logs = db.site_logs
-    date = datetime.datetime.now()
-    ip_address = str(request.remote_addr)
 
-    request_url = 'https://geolocation-db.com/jsonp/' + ip_address
-    ip_response = requests.get(request_url)
-    ip_result = ip_response.content.decode()
-    ip_result = ip_result.split("(")[1].strip(")")
-    print(ip_result)
-    ip_result = json.loads(ip_result)
+    try:
+        site_logs = db.site_logs
+        date = datetime.datetime.now()
+        ip_address = str(request.remote_addr)
 
-    log = {'date': date,
-           'ip_info': ip_result,
-           'referred_page': str(request.referrer),
-           'landing page': str(request.path)}
+        request_url = 'https://geolocation-db.com/jsonp/' + ip_address
+        ip_response = requests.get(request_url)
+        ip_result = ip_response.content.decode()
+        ip_result = ip_result.split("(")[1].strip(")")
+        print(ip_result)
+        ip_result = json.loads(ip_result)
 
-    site_logs.insert_one(log)
+        log = {'date': date,
+               'ip_info': ip_result,
+               'referred_page': str(request.referrer),
+               'landing page': str(request.path)}
+
+        site_logs.insert_one(log)
+    except:
+        pass

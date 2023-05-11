2335a1fdef7a4e0e6f186b59238622f2c32e15f5
kdenny25 <kelsead25@gmail.com>
2023-05-11 09:43:18 -0500

    Updating site logging feature. Checking if a frontend call will pull the correct IP address

--- app.py
+++ app.py
@@ -1150,5 +1150,32 @@ def get_in_touch():
     # ideally this would be a thank you for your comment page
     return redirect('/')
 
-# session info for cookies
+@app.post('/log_visit')
+def log_visit():
+    try:
+        site_logs = db.site_logs
+        date = datetime.datetime.now()
+
+
+        # ip_address = str(request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
+        # request_url = 'http://api.ipapi.com/api/' + ip_address + '?access_key=' + access_key
+        request_url = 'https://ipapi.co/json/'
+        #ip_response = requests.get(request_url)
+
+        ip_result = ip_response.content.decode()
+        # ip_result = ip_result.split("(")[1].strip(")")
+
+        ip_result = json.loads(ip_result)
+        print(ip_result)
+
+
+        log = {'date': date,
+               'ip_info': ip_result,
+               'referred_page': str(request.referrer),
+               'landing page': str(request.path)}
+
+        site_logs.insert_one(log)
+    except:
+        print('error retrieving IP')
+        pass
 
